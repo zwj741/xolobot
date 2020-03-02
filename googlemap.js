aafaf661c889bb4fc449a3a56c5155053268b7b4
rich <rich@chengdeair.home>
2020-03-02 16:11:32 +0100

    changes

--- googlemap.js
+++ googlemap.js
@@ -6,44 +6,48 @@ class GoogleMapService
 {
     constructor()
     {
-
+        this.cache = {};
     }
     validate(address)
     {
         return new Promise(async (resolve)=>{
 
-            var browser = new Browser({
-                headless:true
-            });
-            await browser.goToLink("https://www.google.com/maps/");
-            await browser.page.type("#searchboxinput",address);
-            await browser.page.click("#searchbox-searchbutton");
-            await browser.page.waitFor(3000);
-            var result = null;
-            try{
-                 var htmlString =  await browser.page.$eval(".section-result-descriptions", (element) => {
-                    return element.innerHTML
-                  });
-                  var htmlObject = parser.parse( htmlString);
-                  if(htmlObject.childNodes.length>1)
-                  {
-                      result = [];
-                    for(var i =0;i<htmlObject.childNodes.length;++i)
-                    {
-                        result.push(htmlObject.childNodes[i].text.trim())
-                    }
-                  }
-                
-            }
-            catch
+            if(!this.cache["address"])
             {
-
+                var browser = new Browser({
+                    headless:true
+                });
+                await browser.goToLink("https://www.google.com/maps/");
+                await browser.page.type("#searchboxinput",address);
+                await browser.page.click("#searchbox-searchbutton");
+                await browser.page.waitFor(3000);
+                var result = null;
+                try{
+                     var htmlString =  await browser.page.$eval(".section-result-descriptions", (element) => {
+                        return element.innerHTML
+                      });
+                      var htmlObject = parser.parse( htmlString);
+                      if(htmlObject.childNodes.length>1)
+                      {
+                          result = [];
+                        for(var i =0;i<htmlObject.childNodes.length;++i)
+                        {
+                            result.push(htmlObject.childNodes[i].text.trim())
+                        }
+                      }
+                    
+                }
+                catch
+                {
+    
+                }
+                //console.log(result)
+                await browser.quit();
+                this.cache[address] = result;
             }
-            //console.log(result)
-            await browser.quit()
-            resolve(result);
+            resolve(this.cache[address]);
+
            
-             // console.log(result)
         })
        
     }
